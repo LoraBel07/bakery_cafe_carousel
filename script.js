b6e5e41dc73efd680ef9afbafb7c45b3fcc5a0b5
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2022-10-07 13:47:41 -0400

    commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const items = document.querySelectorAll('.item');
+console.log(items);
+
+items.forEach(item => {
+    item.addEventListener("mouseover", () => {
+
+        removeFocus();
+        item.classList.add('selected');
+    })
+    removeFocus = () => {
+        items.forEach(item =>{
+            item.classList.remove('selected');
+        })
+    }
+})
